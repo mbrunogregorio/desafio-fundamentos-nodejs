553cbe461c4af159860b02003f625a68acc07b4a
Bruno Gregorio <brunomoises@gmail.com>
2020-07-18 00:15:01 -0400

    Challenge done! All tests passed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, element) => {
+      if (element.type === 'income') return (total += element.value);
+      return total;
+    }, 0);
+
+    const outcome = this.transactions.reduce((total, element) => {
+      if (element.type === 'outcome') return (total += element.value);
+      return total;
+    }, 0);
+
+    const total = income - outcome;
+    const balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const checkBalance = this.checkBalance(transaction);
+    if (!checkBalance) {
+      throw Error('Insuficient balance');
+    }
+
+    this.transactions.push(transaction);
+    return transaction;
+  }
+
+  public checkBalance(transaction: Transaction): boolean {
+    // TODO calcular o balance e verificar se o valor é valido, se for returnar true, se nao for retornar false
+    const balance = this.getBalance();
+    if (transaction.type === 'outcome') {
+      return balance.total >= transaction.value;
+    }
+    return true;
   }
 }
 
